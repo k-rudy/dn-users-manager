75f94884812572549b12f1d3854ea5a320140681
Konstantin Rudy <konstantin.rudy@gmail.com>
2014-03-20 13:45:29 +0300

    Added Role#starting_with method

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ActiveRecord::Base
   has_many :users
   has_and_belongs_to_many :permissions
+  
+  scope :starting_with, ->(query){ where("name like ?", "#{query}%") }
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -7,4 +7,16 @@ describe Role do
     it { should have_many(:users) }
     it { should have_and_belong_to_many(:permissions) }
   end
+  
+  describe '#starting_with' do
+    
+    subject { Role }
+    let!(:viewer) { create(:role, name: 'Viewer') }  
+    let!(:user_viewer) { create(:role, name: 'User Viewer') }  
+    let!(:admin_viewer) { create(:role, name: 'Viewer of Admin') }  
+    
+    it 'returns all roles whose name starts with parameter' do
+      expect(subject.starting_with('View')).to match_array([ viewer, admin_viewer ])
+    end
+  end
 end
